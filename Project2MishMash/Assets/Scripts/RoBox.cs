9993c222a548a1f18122e4f04061c38e4c0330e1
Patrick Mitchell (RIT Student) <prm5983@ad.rit.edu>
2019-12-06 15:43:39 -0500

    Imported chair model and changed wavespeed to be more mellow

--- Project2MishMash/Assets/Scripts/RoBox.cs
+++ Project2MishMash/Assets/Scripts/RoBox.cs
@@ -32,120 +32,84 @@ public class RoBox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
+        MoveLogic();
+    }
+
+    private void MoveLogic()
+    {
+        transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position) - 25, transform.position.z);
+
+        //forward and back
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            moveOnTrack = !moveOnTrack;
+            acc = transform.forward * accelScale;
 
-            pos = new Vector3(0f, Terrain.activeTerrain.SampleHeight(new Vector3(0f, 0f, 0f)), 0f);
-            vel = new Vector3(0f, 0f, 0f);
-            acc = new Vector3(0f, 0f, 0f);
-
-            transform.position = pos;
+            vel += acc * Time.deltaTime;
         }
-
-        if (moveOnTrack)
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
-            tTime +=  Time.deltaTime;
-            xFloat = 50.0f * Mathf.Cos(tTime);
-            zFloat = 50.0f * Mathf.Sin(tTime);
-            yFloat = (1f/100f) * xFloat * zFloat;
-
-            //this is another way to get the y value that will work for any heightmap-specified surface of the form y = f(x,z) 
-            //y = Terrain.activeTerrain.SampleHeight(new Vector3(x, 0f, z)) - 25f; //Note that -25f adjusts for the translation of the Terrain GameObject 
-
-            parametric = new Vector3(xFloat, yFloat, zFloat);
-            transform.position = parametric;
-
-            //the gradient to the surface F(x,y,z) = 0 is the normal vector of the local tangent plane to the surface, which here is F(x,y,z) = y - x z
-            gradient = new Vector3(-zFloat/100f, 1, -xFloat/100f);  //the gradient is just the vector of the derivatives < dF/dx, dF/dy, dF/dz >
-          
-            vel = new Vector3(-50.0f * Mathf.Sin(tTime), 25.0f * Mathf.Cos(2.0f * tTime), 50.0f * Mathf.Cos(tTime));
-
-            //this is another way to get the normal vector that will work for any heightmap-specified surface of the form y = f(x,z)
-            //transform.up = Terrain.activeTerrain.GetComponent<TerrainCollider>().terrainData.GetInterpolatedNormal((x+50f)/100f, (z+50f)/100f);
-
-            transform.up = gradient.normalized;
-            //transform.right = Vector3.Cross(gradient.normalized, vel.normalized); //this isn't necessary since Unity computes the 3rd vector given the other 2
-            transform.forward = vel.normalized;
-        }
-
-        //when not moving on track, move with wasd
-        else
-        {
-            transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position) - 25, transform.position.z);
-
-            //forward and back
-            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            if (vel.magnitude >= 0.15f)
             {
-                acc = transform.forward * accelScale;
+                acc = -transform.forward * accelScale;
 
                 vel += acc * Time.deltaTime;
             }
-            else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-            {
-                if (vel.magnitude >= 0.15f)
-                {
-                    acc = -transform.forward * accelScale;
-
-                    vel += acc * Time.deltaTime;
-                }
-                else
-                {
-                    acc = Vector3.zero;
-                }
-            }
-
-            //right and left
-            else if (Input.GetKey(KeyCode.RightArrow) && vel.magnitude >= 0.02f)
-            {
-                acc = transform.right * accelScale;
-
-                speed = vel.magnitude;
-                vel += acc * Time.deltaTime;
-                vel = speed * vel.normalized;
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow) && vel.magnitude >= 0.02f)
-            {
-                acc = -transform.right * accelScale;
-
-                speed = vel.magnitude;
-                vel += acc * Time.deltaTime;
-                vel = speed * vel.normalized;
-            }
-
-            //alt right and left
-            else if (Input.GetKey(KeyCode.D) && vel.magnitude >= 0.02f)
-            {
-                vel = Quaternion.Euler(0, aDRotationAngle, 0) * vel;
-            }
-            else if (Input.GetKey(KeyCode.A) && vel.magnitude >= 0.02f)
-            {
-                vel = Quaternion.Euler(0, -aDRotationAngle, 0) * vel;
-            }
-
             else
             {
                 acc = Vector3.zero;
             }
+        }
 
-            if (vel.magnitude < 0.02f)
-            {
-                vel = Vector3.zero;
-            }
+        //right and left
+        else if (Input.GetKey(KeyCode.RightArrow) && vel.magnitude >= 0.02f)
+        {
+            acc = transform.right * accelScale;
 
-            transform.position += vel * Time.deltaTime;
+            speed = vel.magnitude;
+            vel += acc * Time.deltaTime;
+            vel = speed * vel.normalized;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) && vel.magnitude >= 0.02f)
+        {
+            acc = -transform.right * accelScale;
 
-            //Get the normal of the terrain so we can set the rotation of the object with it.
-            Vector3 terrainNormal = GetTerrainNormal(Terrain.activeTerrain, transform.position);
+            speed = vel.magnitude;
+            vel += acc * Time.deltaTime;
+            vel = speed * vel.normalized;
+        }
 
-            //LookRotation prioritizes the forward vector, so we have to make our forward vector perpendicular
-            //to the normal, thus properly aligning the object with the normal
-            vel = vel - terrainNormal * Vector3.Dot(vel, terrainNormal);
+        //alt right and left
+        else if (Input.GetKey(KeyCode.D) && vel.magnitude >= 0.02f)
+        {
+            vel = Quaternion.Euler(0, aDRotationAngle, 0) * vel;
+        }
+        else if (Input.GetKey(KeyCode.A) && vel.magnitude >= 0.02f)
+        {
+            vel = Quaternion.Euler(0, -aDRotationAngle, 0) * vel;
+        }
 
-            if (vel.magnitude >= 0.02f)
-            {
-                transform.rotation = Quaternion.LookRotation(vel.normalized, terrainNormal);
-            }
+        else
+        {
+            acc = Vector3.zero;
+        }
+
+        if (vel.magnitude < 0.02f)
+        {
+            vel = Vector3.zero;
+        }
+
+        transform.position += vel * Time.deltaTime;
+
+        //Get the normal of the terrain so we can set the rotation of the object with it.
+        Vector3 terrainNormal = GetTerrainNormal(Terrain.activeTerrain, transform.position);
+
+        //LookRotation prioritizes the forward vector, so we have to make our forward vector perpendicular
+        //to the normal, thus properly aligning the object with the normal
+        vel = vel - terrainNormal * Vector3.Dot(vel, terrainNormal);
+
+        if (vel.magnitude >= 0.02f)
+        {
+            transform.rotation = Quaternion.LookRotation(vel.normalized, terrainNormal);
         }
     }
 
@@ -157,7 +121,7 @@ public class RoBox : MonoBehaviour
     /// <param name="terrain">The terrain to get the normal from.</param>
     /// <param name="position">The position on the terrain to get the normal from.</param>
     /// <returns>The normal at the given position on the given terrain.</returns>
-    public Vector3 GetTerrainNormal(Terrain terrain, Vector3 position)
+    private Vector3 GetTerrainNormal(Terrain terrain, Vector3 position)
     {
         //Subtracts the terrain's position from the given position, to get the position relative to the terrain
         Vector3 terrainLocalPos = position - terrain.transform.position;
