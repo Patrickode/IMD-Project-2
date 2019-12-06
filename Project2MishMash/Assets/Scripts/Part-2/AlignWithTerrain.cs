f4209df533d27ef9c0ac96486db577f9fd5a7154
Patrick Mitchell (RIT Student) <prm5983@ad.rit.edu>
2019-12-06 18:17:23 -0500

    Made pawn pieces spawn in center w/ gaussian distribution

---
+++ Project2MishMash/Assets/Scripts/Part-2/AlignWithTerrain.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignWithTerrain : MonoBehaviour
+{
+    Vector3 terrainNormal;
+
+    void Start()
+    {
+        //When this object is spawned, make sure it's level with the terrain.
+        transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position) + 0.5f, transform.position.z);
+
+        //Once level, rotate to be aligned with the terrain.
+        terrainNormal = GetTerrainNormal(Terrain.activeTerrain, transform.position);
+        transform.forward = transform.forward - terrainNormal * Vector3.Dot(transform.forward, terrainNormal);
+        transform.rotation = Quaternion.LookRotation(transform.forward, terrainNormal);
+    }
+
+    /// <summary>
+    /// Takes a terrain and a position, and gets the normal of the terrain at that position.
+    /// Huge thanks to http://answers.unity.com/answers/1295326/view.html for this method; it's been
+    /// slightly edited to fit this script, but the idea of using InverseLerp and whatnot came from there.
+    /// </summary>
+    /// <param name="terrain">The terrain to get the normal from.</param>
+    /// <param name="position">The position on the terrain to get the normal from.</param>
+    /// <returns>The normal at the given position on the given terrain.</returns>
+    private Vector3 GetTerrainNormal(Terrain terrain, Vector3 position)
+    {
+        //Subtracts the terrain's position from the given position, to get the position relative to the terrain
+        Vector3 terrainLocalPos = position - terrain.transform.position;
+
+        //with that relative position, use InverseLerp (get percentage from value) to get how far along the terrain
+        //the relative position is, from 0 to 1
+        Vector2 normalizedPos = new Vector2(
+            Mathf.InverseLerp(0f, terrain.terrainData.size.x, terrainLocalPos.x),
+            Mathf.InverseLerp(0f, terrain.terrainData.size.z, terrainLocalPos.z)
+        );
+
+        //Finally, now that we have the normalized, local position, we can get the interpolated normal at that position
+        var terrainNormal = terrain.terrainData.GetInterpolatedNormal(normalizedPos.x, normalizedPos.y);
+        return terrainNormal;
+    }
+}
---
+++ Project2MishMash/Assets/Scripts/Part-2/RandomizeFloor.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeFloor : MonoBehaviour
+{
+    public GameObject piecePrefab;
+
+    void Start()
+    {
+        PerlinizeTerrain(Terrain.activeTerrain, 0, 0, 0.01f);
+
+        //summon a sizeable number of chess pieces to populate this sparse ocean landscape
+        int numberOfBoys = Random.Range(100, 201);
+        int mean = 0;
+
+        for (int i = 0; i < numberOfBoys; i++)
+        {
+            Instantiate
+                (
+                    piecePrefab,
+                    new Vector3(Gaussian(mean, mean + 49), 0, Gaussian(mean, mean + 49)),
+                    Quaternion.identity
+                );
+        }
+    }
+
+    private void PerlinizeTerrain(Terrain terrainToPerlinize, float xOffset, float yOffset, float spacing)
+    {
+        //Make a height array
+        float[,] heightArray = new float[terrainToPerlinize.terrainData.heightmapResolution, terrainToPerlinize.terrainData.heightmapResolution];
+
+        //Fill the height array
+        float xVal = xOffset;
+        float zVal = yOffset;
+
+        //For each row,
+        for (int i = 0; i < heightArray.GetLength(0); i++)
+        {
+            //reset x val, because we're at a new row
+            xVal = xOffset;
+
+            //for each column,
+            for (int j = 0; j < heightArray.GetLength(0); j++)
+            {
+                //set the height array at our current coordinate to a perlin noise value dictated by x and z vals
+                heightArray[i, j] = Mathf.PerlinNoise(xVal, zVal);
+
+                //move over to the next x coordinate
+                xVal += spacing;
+            }
+
+            //move onto the next z coordinate
+            zVal += spacing;
+        }
+
+        //finally, the terrain heightmap is done; apply it
+        terrainToPerlinize.terrainData.SetHeights(0, 0, heightArray);
+    }
+
+    /// <summary>
+    /// Takes a mean and a standard deviation, and returns a random value according to the bell curve defined by them.
+    /// </summary>
+    /// <param name="mean">The average value of the bell curve.</param>
+    /// <param name="maxDifference">The maximum difference from the mean. Standard deviation is a 4th of this.</param>
+    /// <returns>A random value somewhere on the bell curve; likely within 1 deviation.</returns>
+    private float Gaussian(float mean, float maxDifference)
+    {
+        float val1 = Random.Range(0f, 1f);
+        float val2 = Random.Range(0f, 1f);
+
+        float gaussValue = Mathf.Sqrt(-2.0f * Mathf.Log(val1)) * Mathf.Sin(2.0f * Mathf.PI * val2);
+
+        return mean + (maxDifference / 8) * gaussValue;
+    }
+}
