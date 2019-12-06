49a0111bf2c3b5ce6a95a171f2050406be744084
Patrick Mitchell (RIT Student) <prm5983@ad.rit.edu>
2019-12-06 14:07:52 -0500

    Added TerrainHuggingVehicle code, implemented GetTerrainNormal

---
+++ Project2MishMash/Assets/Scripts/RoBox.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoBox : MonoBehaviour
+{
+    private Vector3 pos;
+    private Vector3 vel;
+    private Vector3 acc;
+    private float gravFloat = 32f;
+    private Vector3 gravity;
+    private bool moveOnTrack = false;
+    private float xFloat;
+    private float yFloat;
+    private float zFloat;
+
+    private Vector3 zero = new Vector3(0f, 0f, 0f);
+    private Vector3 origin = new Vector3(0f, 0f, 0f);
+    private float tTime = 0f;
+    private Vector3 gradient, parametric;
+
+    public float accelScale;
+    public float aDRotationAngle;
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gravity = new Vector3(0, -gravFloat, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            moveOnTrack = !moveOnTrack;
+
+            pos = new Vector3(0f, Terrain.activeTerrain.SampleHeight(new Vector3(0f, 0f, 0f)), 0f);
+            vel = new Vector3(0f, 0f, 0f);
+            acc = new Vector3(0f, 0f, 0f);
+
+            transform.position = pos;
+        }
+
+        if (moveOnTrack)
+        {
+            tTime +=  Time.deltaTime;
+            xFloat = 50.0f * Mathf.Cos(tTime);
+            zFloat = 50.0f * Mathf.Sin(tTime);
+            yFloat = (1f/100f) * xFloat * zFloat;
+
+            //this is another way to get the y value that will work for any heightmap-specified surface of the form y = f(x,z) 
+            //y = Terrain.activeTerrain.SampleHeight(new Vector3(x, 0f, z)) - 25f; //Note that -25f adjusts for the translation of the Terrain GameObject 
+
+            parametric = new Vector3(xFloat, yFloat, zFloat);
+            transform.position = parametric;
+
+            //the gradient to the surface F(x,y,z) = 0 is the normal vector of the local tangent plane to the surface, which here is F(x,y,z) = y - x z
+            gradient = new Vector3(-zFloat/100f, 1, -xFloat/100f);  //the gradient is just the vector of the derivatives < dF/dx, dF/dy, dF/dz >
+          
+            vel = new Vector3(-50.0f * Mathf.Sin(tTime), 25.0f * Mathf.Cos(2.0f * tTime), 50.0f * Mathf.Cos(tTime));
+
+            //this is another way to get the normal vector that will work for any heightmap-specified surface of the form y = f(x,z)
+            //transform.up = Terrain.activeTerrain.GetComponent<TerrainCollider>().terrainData.GetInterpolatedNormal((x+50f)/100f, (z+50f)/100f);
+
+            transform.up = gradient.normalized;
+            //transform.right = Vector3.Cross(gradient.normalized, vel.normalized); //this isn't necessary since Unity computes the 3rd vector given the other 2
+            transform.forward = vel.normalized;
+        }
+
+        //when not moving on track, move with wasd
+        else
+        {
+            transform.position = new Vector3(transform.position.x, Terrain.activeTerrain.SampleHeight(transform.position) - 25, transform.position.z);
+
+            //forward and back
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            {
+                acc = transform.forward * accelScale;
+
+                vel += acc * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            {
+                if (vel.magnitude >= 0.15f)
+                {
+                    acc = -transform.forward * accelScale;
+
+                    vel += acc * Time.deltaTime;
+                }
+                else
+                {
+                    acc = Vector3.zero;
+                }
+            }
+
+            //right and left
+            else if (Input.GetKey(KeyCode.RightArrow) && vel.magnitude >= 0.02f)
+            {
+                acc = transform.right * accelScale;
+
+                speed = vel.magnitude;
+                vel += acc * Time.deltaTime;
+                vel = speed * vel.normalized;
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow) && vel.magnitude >= 0.02f)
+            {
+                acc = -transform.right * accelScale;
+
+                speed = vel.magnitude;
+                vel += acc * Time.deltaTime;
+                vel = speed * vel.normalized;
+            }
+
+            //alt right and left
+            else if (Input.GetKey(KeyCode.D) && vel.magnitude >= 0.02f)
+            {
+                vel = Quaternion.Euler(0, aDRotationAngle, 0) * vel;
+            }
+            else if (Input.GetKey(KeyCode.A) && vel.magnitude >= 0.02f)
+            {
+                vel = Quaternion.Euler(0, -aDRotationAngle, 0) * vel;
+            }
+
+            else
+            {
+                acc = Vector3.zero;
+            }
+
+            if (vel.magnitude < 0.02f)
+            {
+                vel = Vector3.zero;
+            }
+
+            transform.position += vel * Time.deltaTime;
+
+            //Get the normal of the terrain so we can set the rotation of the object with it.
+            Vector3 terrainNormal = GetTerrainNormal(Terrain.activeTerrain, transform.position);
+
+            //LookRotation prioritizes the forward vector, so we have to make our forward vector perpendicular
+            //to the normal, thus properly aligning the object with the normal
+            vel = vel - terrainNormal * Vector3.Dot(vel, terrainNormal);
+
+            if (vel.magnitude >= 0.02f)
+            {
+                transform.rotation = Quaternion.LookRotation(vel.normalized, terrainNormal);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Takes a terrain and a position, and gets the normal of the terrain at that position.
+    /// Huge thanks to http://answers.unity.com/answers/1295326/view.html for this method; it's been
+    /// slightly edited to fit this script, but the idea of using InverseLerp and whatnot came from there.
+    /// </summary>
+    /// <param name="terrain">The terrain to get the normal from.</param>
+    /// <param name="position">The position on the terrain to get the normal from.</param>
+    /// <returns>The normal at the given position on the given terrain.</returns>
+    public Vector3 GetTerrainNormal(Terrain terrain, Vector3 position)
+    {
+        //Subtracts the terrain's position from the given position, to get the position relative to the terrain
+        Vector3 terrainLocalPos = position - terrain.transform.position;
+
+        //with that relative position, use InverseLerp (get percentage from value) to get how far along the terrain
+        //the relative position is, from 0 to 1
+        Vector2 normalizedPos = new Vector2(
+            Mathf.InverseLerp(0f, terrain.terrainData.size.x, terrainLocalPos.x),
+            Mathf.InverseLerp(0f, terrain.terrainData.size.z, terrainLocalPos.z)
+        );
+
+        //Finally, now that we have the normalized, local position, we can get the interpolated normal at that position
+        var terrainNormal = terrain.terrainData.GetInterpolatedNormal(normalizedPos.x, normalizedPos.y);
+        return terrainNormal;
+    }
+}
---
+++ Project2MishMash/Assets/Scripts/TerrainGeneration.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class TerrainGeneration : MonoBehaviour 
+{
+	private TerrainData myTerrainData;
+	public Vector3 worldSize;
+	public int resolution = 129;			// number of vertices along X and Z axes
+	float[,] heightArray;
+
+    public GameObject mainCamera;
+    public GameObject vehicle;
+
+	void Start () 
+	{
+		myTerrainData = gameObject.GetComponent<TerrainCollider> ().terrainData;
+		worldSize = new Vector3 (100, 100, 100);
+		myTerrainData.size = worldSize;
+		myTerrainData.heightmapResolution = resolution;
+		heightArray = new float[resolution, resolution];
+
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            PotatoChip();  //aka hyperbolic paraboloid
+        }
+        else
+        {
+            Perlin();
+        }
+
+		// Assign values from heightArray into the terrain object's heightmap
+		myTerrainData.SetHeights(0, 0, heightArray);
+
+        mainCamera?.transform.Translate(new Vector3(0, myTerrainData.size.y - 10, 0));
+	}
+	
+
+	void Update () 
+	{
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        mainCamera?.transform.LookAt(vehicle?.transform);
+    }
+
+    private void OnGUI()
+    {
+        GUI.skin.box.wordWrap = true;
+
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            GUI.Box
+                (
+                    new Rect(10, 10, 150, 100),
+                    string.Format("Current Scene\n1: Potato Chip\n\nPress 1 or 2 to swap to that scene.")
+                );
+        }
+        else
+        {
+            GUI.Box
+                (
+                    new Rect(10, 10, 150, 100),
+                    string.Format("Current Scene\n2: Perlin\n\nPress 1 or 2 to swap to that scene.")
+                );
+        }
+    }
+
+
+    void PotatoChip()  //generate a surface known as a hyperbolic paraboloid
+    {
+        // Fill heightArray with values
+        for (int i = 0; i < resolution; i++)
+        {
+            for (int j = 0; j < resolution; j++)
+            {
+                // Must cast either j or resolution to a float, otherwise integer division occurs
+                heightArray[i, j] = 0.25f + (float)(i-(resolution-1)/2)/(float)(resolution-1) * (float)(j-(resolution-1)/2)/(float)(resolution-1) ;
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Perlin()
+    /// Assigns heightArray values using Perlin noise
+    /// </summary>
+    void Perlin()
+	{
+
+        // Perlin noise value - this is the starting "index" where the PerlinNoise function
+
+        float originate = Random.Range(0.0f, 100.0f);
+
+        float xIndex = originate; 
+        float yIndex = originate; 
+
+            // Fill heightArray with Perlin generated values
+
+            for (int i = 0; i < resolution; i++)
+            {
+                for (int j = 0; j < resolution; j++)
+                {
+                 
+                    yIndex += 0.020f;
+                    heightArray[i, j] = Mathf.PerlinNoise(xIndex, yIndex);
+                }
+
+     
+            yIndex = originate;
+
+                xIndex += .020f;
+            }
+     
+    }
+}
