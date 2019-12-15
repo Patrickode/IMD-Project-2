6e132a0035c4692f5401b34c1a9172864d0f0b5a
Patrick Mitchell <prm5983@rit.edu>
2019-12-15 17:35:34 -0500

    Finished Part 7

---
+++ Project2MishMash/Assets/Scripts/Part-7/PainedVibration.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PainedVibration : MonoBehaviour
+{
+    private Vector3 originalPos;
+
+    public float jitterAmount;
+
+    void Start()
+    {
+        originalPos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 jitterDirection = Random.insideUnitSphere * jitterAmount;
+        transform.position = (originalPos + jitterDirection);
+
+        transform.position = new Vector3
+            (
+                transform.position.x,
+                Mathf.Clamp(transform.position.y, 0.7f, Mathf.Infinity),
+                transform.position.z
+            );
+    }
+}
--- Project2MishMash/Assets/Scripts/Part-7/RotateBoy.cs
+++ Project2MishMash/Assets/Scripts/Part-7/RotateBoy.cs
@@ -5,13 +5,9 @@ using UnityEngine;
 public class RotateBoy : MonoBehaviour
 {
     public float rotateSpeed;
-    public GameObject cam;
-    public GameObject victim;
 
     void Update()
     {
         transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
-
-        cam.transform.LookAt(victim.transform);
     }
 }
