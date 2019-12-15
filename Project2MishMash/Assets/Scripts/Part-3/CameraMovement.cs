1244e7df41567d6612d6e1f326f209ecf2847f6e
Patrick Mitchell <prm5983@rit.edu>
2019-12-15 18:43:05 -0500

    Implemented Camera Movement

---
+++ Project2MishMash/Assets/Scripts/Part-3/CameraMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        //Up and Down
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime, Space.World);
+        }
+
+        //Left and Right
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0, Space.World);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0, Space.World);
+        }
+    }
+}
