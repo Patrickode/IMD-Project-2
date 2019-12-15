ef797ddfbbe7b122025b3bb689fc126dabcea117
Patrick Mitchell <prm5983@rit.edu>
2019-12-15 17:21:47 -0500

    Added music and boy spin to Part 7

---
+++ Project2MishMash/Assets/Scripts/Part-7/RotateBoy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBoy : MonoBehaviour
+{
+    public float rotateSpeed;
+    public GameObject cam;
+    public GameObject victim;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+
+        cam.transform.LookAt(victim.transform);
+    }
+}
