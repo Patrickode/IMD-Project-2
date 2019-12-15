ed9e0643118b9c68c95b326e50f4d153e4aaf381
Patrick Mitchell <prm5983@rit.edu>
2019-12-15 18:57:05 -0500

    Added GUI to Parts 3 and 4

--- Project2MishMash/Assets/Scripts/Part-3-4/CameraMovement.cs
+++ Project2MishMash/Assets/Scripts/Part-3-4/CameraMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public float speed;
+    private bool isPart3 = true;
 
     void Update()
     {
@@ -26,6 +27,23 @@ public class CameraMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(speed * Time.deltaTime, 0, 0, Space.World);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isPart3 = !isPart3;
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (isPart3)
+        {
+            GUI.Box(new Rect(10, 10, 175, 175), "Part 3\nMax/Min\nLeft/Right\n\nPress Space to\nswitch between\nparts 3 & 4.");
+        }
+        else
+        {
+            GUI.Box(new Rect(10, 10, 175, 175), "Part 4\nNorth/South\nEast/West\n\nPress Space to\nswitch between\nparts 3 & 4.");
         }
     }
 }
