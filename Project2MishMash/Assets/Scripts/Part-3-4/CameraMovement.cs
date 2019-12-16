37d50a9fc52d7bb9bd44bebf31cfde3fac5eff66
Patrick Mitchell <prm5983@rit.edu>
2019-12-15 20:49:24 -0500

    Added cam rotation

--- Project2MishMash/Assets/Scripts/Part-3-4/CameraMovement.cs
+++ Project2MishMash/Assets/Scripts/Part-3-4/CameraMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public float speed;
+    public float rotateSpeed;
     private bool isPart3 = true;
 
     void Update()
@@ -20,11 +21,11 @@ public class CameraMovement : MonoBehaviour
         }
 
         //Left and Right
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(-speed * Time.deltaTime, 0, 0, Space.World);
         }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(speed * Time.deltaTime, 0, 0, Space.World);
         }
@@ -37,6 +38,16 @@ public class CameraMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(0, -speed * Time.deltaTime, 0, Space.World);
+        }
+
+        //Rotate Camera
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(transform.up, -rotateSpeed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(transform.up, rotateSpeed);
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
